9d8d240d68cd4e79b0b7c279f9a51650a17b54e3
Vladimir Manzhula <vova8246@gmail.com>
2024-05-29 19:37:03 +0500

    Init project

---
+++ Assets/Scripts/Installers/Game/GameUiInstaller.cs
@@ -0,0 +1,24 @@
+﻿using SimpleUi;
+using Ui.Game.Hud;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Zenject;
+
+namespace Installers.Game
+{
+    [CreateAssetMenu(menuName = "Installers/GameUiInstaller", fileName = "GameUiInstaller")]
+    public class GameUiInstaller : ScriptableObjectInstaller
+    {
+        [SerializeField] private Canvas canvas;
+        [FormerlySerializedAs("hudView")] [SerializeField] private MenuView menuView;
+        
+
+        public override void InstallBindings()
+        {
+            var canvasInstance = Container.InstantiatePrefabForComponent<Canvas>(canvas);
+            var canvasTransform = canvasInstance.transform;
+            
+            Container.BindUiView<MenuController, MenuView>(menuView, canvasTransform);
+        }
+    }
+}
---
+++ Assets/Scripts/Ui/Game/Windows/MenuWindow.cs
@@ -0,0 +1,16 @@
+﻿using Core.Constants;
+using SimpleUi;
+using Ui.Game.Hud;
+
+namespace Ui.Game.Windows
+{
+    public class MenuWindow : WindowBase
+    {
+        public override string Name => nameof(WindowNames.Game.menu);
+        
+        protected override void AddControllers()
+        {
+            AddController<MenuController>();
+        }
+    }
+}
