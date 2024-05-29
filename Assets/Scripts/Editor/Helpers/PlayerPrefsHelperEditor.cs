d3dfde4b8ff590301090e761521292768b060b59
Vladimir Manzhula <vova8246@gmail.com>
2024-05-29 21:42:51 +0500

    PT-4 Added data cleanup in Tools

---
+++ Assets/Scripts/Editor/Helpers/PlayerPrefsHelperEditor.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.Helpers
+{
+    public class PlayerPrefsHelperEditor
+    {
+        [MenuItem("Tools/SavedGameData/Show PersistentDataPath")]
+        public static void OpenPersistentDataPath() => EditorUtility.RevealInFinder(Application.persistentDataPath);
+        
+        [MenuItem("Tools/SavedGameData/Delete PlayerPrefs")]
+        public static void PlayerPrefsDeleteAll()
+        {
+            PlayerPrefs.DeleteAll();
+            UnityEngine.Debug.Log($"[{nameof(PlayerPrefsHelperEditor)}] [✖_✖] Delete all PlayerPrefs");
+        }
+
+        [MenuItem("Tools/SavedGameData/Delete data in PersistentDataPath")]
+        public static void DeleteGameDataInPersistentDataPath()
+        {
+            var pathData = Application.persistentDataPath;
+            var files = Directory.GetFiles(pathData);
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                fileInfo.Delete();
+            }
+
+            UnityEngine.Debug.Log($"[{nameof(PlayerPrefsHelperEditor)}] [✖_✖] Delete all in dir: {pathData}");
+        }
+
+        [MenuItem("Tools/SavedGameData/DeleteAll #&d")]
+        public static void DeleteAll()
+        {
+            PlayerPrefsDeleteAll();
+            DeleteGameDataInPersistentDataPath();
+        }
+    }
+}
