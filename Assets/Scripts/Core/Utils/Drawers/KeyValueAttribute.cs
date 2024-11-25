0706a5693ab9607169e7803d2e892746110e98c0
Vladimir Manzhula <vova8246@gmail.com>
2024-11-25 12:23:37 +0500

    ST-16 Added the KeyValueAttribute class to display in the Name Editor. Moved the TextMeshPro folder.

---
+++ Assets/Scripts/Core/Utils/Drawers/KeyValueAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Core.Utils.Drawers
+{
+    public class KeyValueAttribute : PropertyAttribute
+    {
+        public readonly string Format;
+        public readonly string[] FieldNames;
+
+        public KeyValueAttribute(params string[] fieldNames)
+        {
+            FieldNames = fieldNames;
+            Format = string.Join(" ", fieldNames.Select((s, i) => "{" + i + "}"));
+        }
+
+        public KeyValueAttribute(string format, string[] fieldNames)
+        {
+            FieldNames = fieldNames;
+            Format = format;
+        }
+    }
+}
