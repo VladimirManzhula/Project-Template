18a5162f4e1fc75596ca05970747cd49e62595b6
Vladimir Manzhula <vova8246@gmail.com>
2025-01-01 16:58:56 +0500

    Add RandomShuffler

---
+++ Assets/Plugins/PdUtils/RandomProvider/Impl/RandomShuffler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PdUtils.RandomProvider.Impl
+{
+    public static class RandomShuffler
+    {
+        private static Random _random = new();
+        
+        public static void Random<T>(List<T> list)
+        {
+            _random = new Random();
+            var count = list.Count;
+            for (var i = count - 1; i > 0; i--)
+            {
+                var j = _random.Next(i + 1);
+                (list[i], list[j]) = (list[j], list[i]);
+            }
+        }
+    }
+}
